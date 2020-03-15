ffb52156c906d37e4fccbb9e88876b4279781755
Matheus Sousa <matheus.ads.study@gmail.com>
2020-03-15 14:38:25 -0300

    # 03 - React com redux 3/4

--- exemplo/src/App.js
+++ exemplo/src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 import './App.css';
 
+const reducer = function(state, action) {
 
+  if(action.type === "INC") {
+    console.log("Incrementando...");
+    return state + action.payload;
+
+  } else if(action.type ==="DESC") {
+    console.log("Incrementando...");
+    return state - action.payload;
+  }
+
+  return state;
+}
+
+const store = createStore(reducer,0);
+
+store.subscribe(function() {
+  console.log("O estado mudou!", store.getState());
+})
+
+store.dispatch({ type: 'INC', payload:3 });
+store.dispatch({ type: 'INC', payload:3 });
+store.dispatch({ type: 'INC', payload:3 });
+store.dispatch({ type: 'DESC', payload:3 });
 
 export default class App extends Component {
 
